af89fca85649fcdcbf332500e466a351fcd15ba5
FelipeHiroshi13 <felipehiroshi13@gmail.com>
2023-01-31 22:44:26 -0400

    first commit

---
+++ lib/blocs/theme/theme_state.dart
@@ -0,0 +1,29 @@
+part of 'theme_bloc.dart';
+
+enum AppTheme { light, dark }
+
+class ThemeState extends Equatable {
+  final AppTheme appTheme;
+
+  ThemeState({
+    this.appTheme = AppTheme.light,
+  });
+
+  factory ThemeState.initial() {
+    return ThemeState();
+  }
+
+  @override
+  List<Object> get props => [appTheme];
+
+  ThemeState copyWith({
+    AppTheme? appTheme,
+  }) {
+    return ThemeState(
+      appTheme: appTheme ?? this.appTheme,
+    );
+  }
+
+  @override
+  bool get stringify => true;
+}
